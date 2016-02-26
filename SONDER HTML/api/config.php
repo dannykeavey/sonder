cb44e986d7ae344cc0a7cd93d25c7ced5c06b858
dannykeavey <dannykeavey@gmail.com>
2016-02-26 02:21:26 +0000

    Added files via upload

---
+++ SONDER HTML/api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'OMG7ox4IBNkoCJjpygMz8FHdD');
+    define('CONSUMER_SECRET', '1KAPsSETBr7tsOfoeYF7lX5hzokq71zTSudPO3UrvaIFngK9Ex');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '514196925-RRfuwP4JewXHMlnhRezCuyXhjk1q6ofmiLni2lPh');
+    define('ACCESS_SECRET', 'UsFkcCwiAlOk4gQrfIwYKRgseFyyaFHvXnVJIvIH82e8n');
